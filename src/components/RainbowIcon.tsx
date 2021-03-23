03bf88707dc3b025f8474e56b9144baba92feb3a
Joonas Palosuo <joonas.palosuo@gmail.com>
2021-03-23 09:57:41 +0200

    fix: reactify svg props

--- src/components/RainbowIcon.tsx
+++ src/components/RainbowIcon.tsx
@@ -1,10 +1,10 @@
 export const RainbowIconGrayscale = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none">
-    <path d="M12 21.5C12 14 14 12 21.5 12" stroke="#474747" stroke-width="2.5" />
-    <path d="M10 21.5C10 12 13 10 21.5 10" stroke="#5C5C5C" stroke-width="2.5" />
-    <path d="M8 21.5C8 11.5 11.5 8 21.5 8" stroke="#A5A5A5" stroke-width="2.5" />
-    <path d="M6 21.5C6 9.5 9.5 6 21.5 6" stroke="#838383" stroke-width="2.5" />
-    <path d="M4 21.5C3.5 8 8 3.5 21.5 4" stroke="#595959" stroke-width="2.5" />
+    <path d="M12 21.5C12 14 14 12 21.5 12" stroke="#474747" strokeWidth="2.5" />
+    <path d="M10 21.5C10 12 13 10 21.5 10" stroke="#5C5C5C" strokeWidth="2.5" />
+    <path d="M8 21.5C8 11.5 11.5 8 21.5 8" stroke="#A5A5A5" strokeWidth="2.5" />
+    <path d="M6 21.5C6 9.5 9.5 6 21.5 6" stroke="#838383" strokeWidth="2.5" />
+    <path d="M4 21.5C3.5 8 8 3.5 21.5 4" stroke="#595959" strokeWidth="2.5" />
     <path
       d="M13 22C13.55 22 14 21.55 14 21C14 18 14.5 16.4 15.46 15.46C16.41 14.5 18 14 21 14C21.27 14 21.5 13.9 21.7 13.7C21.9 13.5 22 13.26 22 13L22 3C22 2.5 21.6 2 21 2C14.2 1.75 9.22 2.86 6 6C2.86 9.22 1.75 14.2 2 21C2 21.57 2.46 22 3 22H13Z"
       stroke="currentColor"
@@ -16,11 +16,11 @@ export const RainbowIconGrayscale = () => (
 
 export const RainbowIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none">
-    <path d="M12 21.5C12 14 14 12 21.5 12" stroke="#002E7D" stroke-width="2.5" />
-    <path d="M10 21.5C10 12 13 10 21.5 10" stroke="#007336" stroke-width="2.5" />
-    <path d="M8 21.5C8 11.5 11.5 8 21.5 8" stroke="#FFD500" stroke-width="2.5" />
-    <path d="M6 21.5C6 9.5 9.5 6 21.5 6" stroke="#FF5800" stroke-width="2.5" />
-    <path d="M4 21.5C3.5 8 8 3.5 21.5 4" stroke="#D42E12" stroke-width="2.5" />
+    <path d="M12 21.5C12 14 14 12 21.5 12" stroke="#002E7D" strokeWidth="2.5" />
+    <path d="M10 21.5C10 12 13 10 21.5 10" stroke="#007336" strokeWidth="2.5" />
+    <path d="M8 21.5C8 11.5 11.5 8 21.5 8" stroke="#FFD500" strokeWidth="2.5" />
+    <path d="M6 21.5C6 9.5 9.5 6 21.5 6" stroke="#FF5800" strokeWidth="2.5" />
+    <path d="M4 21.5C3.5 8 8 3.5 21.5 4" stroke="#D42E12" strokeWidth="2.5" />
     <path
       d="M13 22C13.55 22 14 21.55 14 21C14 18 14.5 16.4 15.46 15.46C16.41 14.5 18 14 21 14C21.27 14 21.5 13.9 21.7 13.7C21.9 13.5 22 13.26 22 13L22 3C22 2.5 21.6 2 21 2C14.2 1.75 9.22 2.86 6 6C2.86 9.22 1.75 14.2 2 21C2 21.57 2.46 22 3 22H13Z"
       stroke="currentColor"
