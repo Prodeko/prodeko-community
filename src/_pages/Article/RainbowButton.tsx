bbfc48b9ee04cfcd24946961d5c922ec78717669
Joonas Palosuo <joonas.palosuo@gmail.com>
2021-03-25 22:30:17 +0200

    feat: rainbow liking

--- src/_pages/Article/RainbowButton.tsx
+++ src/_pages/Article/RainbowButton.tsx
@@ -1,6 +1,6 @@
 import { likeArticle, unlikeArticle } from 'api';
 import { useAuth } from 'api/useAuth';
-import { RainbowIcon, RainbowIconGrayscale } from 'components/RainbowIcon';
+import { RainbowIcon, RainbowIconGrayscale, AnimateIcon } from 'components/RainbowIcon';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import styled, { css } from 'styled-components';
@@ -60,9 +60,9 @@ export const RainbowButton: React.FC<RainbowButtonProps> = ({
         disabled={loading}
         onClick={onClick}
       >
-        <IconWrapper>
-          {isPressed || loading ? <RainbowIcon /> : <RainbowIconGrayscale />}
-        </IconWrapper>
+        <RainbowWrapper className={isPressed || loading ? 'active' : ''}>
+          <AnimateIcon />
+        </RainbowWrapper>
         {children}
       </RainbowButtonWrapper>
     );
@@ -80,6 +80,7 @@ const RainbowButtonWrapper = styled.button<{ loading?: boolean }>`
   border: none;
   display: flex;
   align-items: center;
+  padding: 0;
 
   font-size: 1.25em;
   font-weight: 600;
@@ -97,43 +98,236 @@ const RainbowButtonWrapper = styled.button<{ loading?: boolean }>`
 
   & > span {
     margin-right: 0.5em;
-    font-size: 1.5em;
+    font-size: 2em;
     transition: transform 0.3s ease;
-  }
-
-  /* Fancy animation stuff */
-  &[aria-pressed='true'] > span {
-    transition-delay: 2s;
-  }
-  &[aria-pressed='true']:not(:hover) > span {
-    transition-delay: 0s;
-  }
-
-  &:hover > span {
-    transform: rotate(-5deg) scale(1.1);
-  }
-
-  &:active > span {
-    transition: transform 0.15s ease;
-    transform: rotate(5deg) scale(0.95);
-  }
-
-  &[aria-pressed='true']:hover > span {
-    transform: rotate(5deg) scale(1.05);
-  }
-
-  &[aria-pressed='true']:active > span {
-    transition: transform 0.15s ease;
-    transform: rotate(-5deg) scale(0.9);
-  }
-
-  &&[disabled] > span {
-    transform: unset;
-    transition-delay: 0s;
   }
 `;
 
-const IconWrapper = styled.span`
+const rainbowAnimation = css`
+  svg {
+    cursor: pointer;
+    overflow: visible;
+    #rainbow {
+      filter: grayscale(1);
+      transform-origin: center;
+      animation: animateHeartOut 0.2s linear forwards;
+    }
+    #main-circ {
+      transform-origin: 29.5px 29.5px;
+    }
+  }
+
+  &.active svg {
+    #rainbow {
+      filter: unset;
+      transform: scale(0);
+      animation: animateHeart 0.3s linear forwards 0.25s;
+    }
+    #main-circ {
+      transition: all 2s;
+      animation: animateCircle 0.3s linear forwards;
+      opacity: 1;
+    }
+    #grp1 {
+      opacity: 1;
+      transition: 0.1s all 0.3s;
+      #oval1 {
+        transform: scale(0) translate(0, -30px);
+        transform-origin: 0 0 0;
+        transition: 0.5s transform 0.3s;
+      }
+      #oval2 {
+        transform: scale(0) translate(10px, -50px);
+        transform-origin: 0 0 0;
+        transition: 1.5s transform 0.3s;
+      }
+    }
+    #grp2 {
+      opacity: 1;
+      transition: 0.1s all 0.3s;
+      #oval1 {
+        transform: scale(0) translate(30px, -15px);
+        transform-origin: 0 0 0;
+        transition: 0.5s transform 0.3s;
+      }
+      #oval2 {
+        transform: scale(0) translate(60px, -15px);
+        transform-origin: 0 0 0;
+        transition: 1.5s transform 0.3s;
+      }
+    }
+    #grp3 {
+      opacity: 1;
+      transition: 0.1s all 0.3s;
+      #oval1 {
+        transform: scale(0) translate(30px, 0px);
+        transform-origin: 0 0 0;
+        transition: 0.5s transform 0.3s;
+      }
+      #oval2 {
+        transform: scale(0) translate(60px, 10px);
+        transform-origin: 0 0 0;
+        transition: 1.5s transform 0.3s;
+      }
+    }
+    #grp4 {
+      opacity: 1;
+      transition: 0.1s all 0.3s;
+      #oval1 {
+        transform: scale(0) translate(30px, 15px);
+        transform-origin: 0 0 0;
+        transition: 0.5s transform 0.3s;
+      }
+      #oval2 {
+        transform: scale(0) translate(40px, 50px);
+        transform-origin: 0 0 0;
+        transition: 1.5s transform 0.3s;
+      }
+    }
+    #grp5 {
+      opacity: 1;
+      transition: 0.1s all 0.3s;
+      #oval1 {
+        transform: scale(0) translate(-10px, 20px);
+        transform-origin: 0 0 0;
+        transition: 0.5s transform 0.3s;
+      }
+      #oval2 {
+        transform: scale(0) translate(-60px, 30px);
+        transform-origin: 0 0 0;
+        transition: 1.5s transform 0.3s;
+      }
+    }
+    #grp6 {
+      opacity: 1;
+      transition: 0.1s all 0.3s;
+      #oval1 {
+        transform: scale(0) translate(-30px, 0px);
+        transform-origin: 0 0 0;
+        transition: 0.5s transform 0.3s;
+      }
+      #oval2 {
+        transform: scale(0) translate(-60px, -5px);
+        transform-origin: 0 0 0;
+        transition: 1.5s transform 0.3s;
+      }
+    }
+    #grp7 {
+      opacity: 1;
+      transition: 0.1s all 0.3s;
+      #oval1 {
+        transform: scale(0) translate(-30px, -15px);
+        transform-origin: 0 0 0;
+        transition: 0.5s transform 0.3s;
+      }
+      #oval2 {
+        transform: scale(0) translate(-55px, -30px);
+        transform-origin: 0 0 0;
+        transition: 1.5s transform 0.3s;
+      }
+    }
+    #grp2 {
+      opacity: 1;
+      transition: 0.1s opacity 0.3s;
+    }
+    #grp3 {
+      opacity: 1;
+      transition: 0.1s opacity 0.3s;
+    }
+    #grp4 {
+      opacity: 1;
+      transition: 0.1s opacity 0.3s;
+    }
+    #grp5 {
+      opacity: 1;
+      transition: 0.1s opacity 0.3s;
+    }
+    #grp6 {
+      opacity: 1;
+      transition: 0.1s opacity 0.3s;
+    }
+    #grp7 {
+      opacity: 1;
+      transition: 0.1s opacity 0.3s;
+    }
+  }
+
+  @keyframes animateCircle {
+    40% {
+      transform: scale(10);
+      opacity: 1;
+      fill: #dd4688;
+    }
+    55% {
+      transform: scale(11);
+      opacity: 1;
+      fill: #d46abf;
+    }
+    65% {
+      transform: scale(12);
+      opacity: 1;
+      fill: #cc8ef5;
+    }
+    75% {
+      transform: scale(13);
+      opacity: 1;
+      fill: transparent;
+      stroke: #cc8ef5;
+      stroke-width: 0.5;
+    }
+    85% {
+      transform: scale(17);
+      opacity: 1;
+      fill: transparent;
+      stroke: #cc8ef5;
+      stroke-width: 0.2;
+    }
+    95% {
+      transform: scale(18);
+      opacity: 1;
+      fill: transparent;
+      stroke: #cc8ef5;
+      stroke-width: 0.1;
+    }
+    100% {
+      transform: scale(19);
+      opacity: 1;
+      fill: transparent;
+      stroke: #cc8ef5;
+      stroke-width: 0;
+    }
+  }
+
+  @keyframes animateHeart {
+    0% {
+      transform: scale(0.2);
+    }
+    40% {
+      transform: scale(1.2);
+      animation-timing-function: ease;
+    }
+    100% {
+      animation-timing-function: ease;
+      transform: scale(1);
+    }
+  }
+
+  @keyframes animateHeartOut {
+    0% {
+      transform: scale(1);
+    }
+    20% {
+      transform: scale(0.6);
+      animation-timing-function: ease-out;
+    }
+    100% {
+      animation-timing-function: ease-in;
+      transform: scale(1);
+    }
+  }
+`;
+
+const RainbowWrapper = styled.span`
   display: flex;
-  will-change: transform;
+  ${rainbowAnimation}
 `;
--- src/components/RainbowIcon.tsx
+++ src/components/RainbowIcon.tsx
@@ -29,3 +29,62 @@ export const RainbowIcon = () => (
     />
   </svg>
 );
+
+export const AnimateIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 58 57">
+    <g id="Group" fill="none" fill-rule="evenodd">
+      <g id="rainbow">
+        <g transform="scale(1.5) translate(7 7)">
+          <path d="M12 21.5C12 14 14 12 21.5 12" stroke="#002E7D" strokeWidth="2.5" />
+          <path d="M10 21.5C10 12 13 10 21.5 10" stroke="#007336" strokeWidth="2.5" />
+          <path d="M8 21.5C8 11.5 11.5 8 21.5 8" stroke="#FFD500" strokeWidth="2.5" />
+          <path d="M6 21.5C6 9.5 9.5 6 21.5 6" stroke="#FF5800" strokeWidth="2.5" />
+          <path d="M4 21.5C3.5 8 8 3.5 21.5 4" stroke="#D42E12" strokeWidth="2.5" />
+          <path
+            d="M13 22C13.55 22 14 21.55 14 21C14 18 14.5 16.4 15.46 15.46C16.41 14.5 18 14 21 14C21.27 14 21.5 13.9 21.7 13.7C21.9 13.5 22 13.26 22 13L22 3C22 2.5 21.6 2 21 2C14.2 1.75 9.22 2.86 6 6C2.86 9.22 1.75 14.2 2 21C2 21.57 2.46 22 3 22H13Z"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinejoin="round"
+          />
+        </g>
+      </g>
+
+      <circle id="main-circ" fill="#E2264D" opacity="0" cx="29.5" cy="29.5" r="1.5" />
+
+      <g id="grp7" opacity="0" transform="translate(7 6)">
+        <circle id="oval1" fill="#9CD8C3" cx="2" cy="6" r="2" />
+        <circle id="oval2" fill="#8CE8C3" cx="5" cy="2" r="2" />
+      </g>
+
+      <g id="grp6" opacity="0" transform="translate(0 28)">
+        <circle id="oval1" fill="#CC8EF5" cx="2" cy="7" r="2" />
+        <circle id="oval2" fill="#91D2FA" cx="3" cy="2" r="2" />
+      </g>
+
+      <g id="grp3" opacity="0" transform="translate(52 28)">
+        <circle id="oval2" fill="#9CD8C3" cx="2" cy="7" r="2" />
+        <circle id="oval1" fill="#8CE8C3" cx="4" cy="2" r="2" />
+      </g>
+
+      <g id="grp2" opacity="0" transform="translate(44 6)">
+        <circle id="oval2" fill="#CC8EF5" cx="5" cy="6" r="2" />
+        <circle id="oval1" fill="#CC8EF5" cx="2" cy="2" r="2" />
+      </g>
+
+      <g id="grp5" opacity="0" transform="translate(14 50)">
+        <circle id="oval1" fill="#91D2FA" cx="6" cy="5" r="2" />
+        <circle id="oval2" fill="#91D2FA" cx="2" cy="2" r="2" />
+      </g>
+
+      <g id="grp4" opacity="0" transform="translate(35 50)">
+        <circle id="oval1" fill="#F48EA7" cx="6" cy="5" r="2" />
+        <circle id="oval2" fill="#F48EA7" cx="2" cy="2" r="2" />
+      </g>
+
+      <g id="grp1" opacity="0" transform="translate(24)">
+        <circle id="oval1" fill="#9FC7FA" cx="2.5" cy="3" r="2" />
+        <circle id="oval2" fill="#9FC7FA" cx="7.5" cy="2" r="2" />
+      </g>
+    </g>
+  </svg>
+);
