542962631389b308132852a3c87223917e133cb7
Joonas Palosuo <joonas.palosuo@gmail.com>
2021-04-05 12:12:20 +0300

    fix: un-rename compose but keep docker

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ const withTM = require('next-transpile-modules')(['@directus/sdk-js']);
 
 module.exports = withTM({
   images: {
-    domains: ['localhost', 'talks_cms'],
+    domains: ['localhost', 'talks-cms'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
